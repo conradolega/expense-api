c9fa9b5a740b2444d27e4fbd1ec35db3a823bdd3
conradolega <conradolega@gmail.com>
2014-08-03 21:20:34 +0800

    Initial /expenses API
    Supports GET all and POST

--- expense_api.rb
+++ expense_api.rb
@@ -9,5 +9,24 @@ module ExpenseApp
     get do
       {hello: 'world'}
     end
+
+    resource :expenses do
+      get do
+        {
+          total: Expense.count,
+          expenses: Expense.all.map do |i|
+                      {item: i.item, amount: i.amount}
+                    end
+        }
+      end
+
+      params do
+        requires :amount, type: Integer
+        requires :item, type: String
+      end
+      post do
+        expense = Expense.insert(amount: params[:amount], item: params[:item])
+      end
+    end
   end
 end
